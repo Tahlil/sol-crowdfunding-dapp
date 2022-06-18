d18af03cb973c2b2302a150d8eb50c4aa86aff2f
tahlil <bsse0803@iit.du.ac.bd>
2022-06-18 11:10:12 +0600

    Add campaign create function

--- programs/solcrowdfunding/src/lib.rs
+++ programs/solcrowdfunding/src/lib.rs
@@ -6,10 +6,13 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod solcrowdfunding {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn cerate(ctx: Context<Create>, name: String, description: String) -> Result<()> {
+        let campaign  = &mut ctx.accounts.campaign;
+        campaign.name = nanme;
+        campaign.description = description;
+        campaign.amount_donated = 0;
+        campaign.amdmin = *ctx.accounts.user.key;
         Ok(())
     }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
