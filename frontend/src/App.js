0baa913ad9b4d88c9831508d46866d34467e7008
tahlil <bsse0803@iit.du.ac.bd>
2022-06-18 14:42:21 +0600

    Add connect wallet button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,31 +2,36 @@ import './App.css';
 import { useEffect } from "react";
 
 const App = () => {
-    const checkIfWalletIsConnected = async() => {
-        try {
-            const { solana } = window;
-            if (solana) {
-                if (solana.isPhantom) {
-                    console.log("Phantom wallet found");
-                    const response = await solana.connect({
-                        onlyIfTrusted: true
-                    });
-                    console.log("Connected with public key: " + response.publicKey.toString());
+        const checkIfWalletIsConnected = async() => {
+            try {
+                const { solana } = window;
+                if (solana) {
+                    if (solana.isPhantom) {
+                        console.log("Phantom wallet found");
+                        const response = await solana.connect({
+                            onlyIfTrusted: true
+                        });
+                        console.log("Connected with public key: " + response.publicKey.toString());
+                    }
+                } else {
+                    alert("Solana obj not found, get a Phantom wallet")
                 }
-            } else {
-                alert("Solana obj not found, get a Phantom wallet")
+            } catch (error) {
+                console.error(error);
             }
-        } catch (error) {
-            console.error(error);
-        }
-    };
-    useEffect(() => {
-        const onload = async() => {
-            await checkIfWalletIsConnected();
-        }
-        window.addEventListener('load', onload)
-        return () => window.removeEventListener('load', onload)
-    }, [])
-}
+        };
 
-export default App;
+        const connectWallet = async() => {
+
+        }
+        const renderNotConnectedContainer = () => ( < button onClick = { connectWallet } > Connect to Wallet </button>)
+            useEffect(() => {
+                const onload = async() => {
+                    await checkIfWalletIsConnected();
+                }
+                window.addEventListener('load', onload)
+                return () => window.removeEventListener('load', onload)
+            }, [])
+        }
+
+        export default App;
