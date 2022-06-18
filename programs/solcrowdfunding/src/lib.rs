e922d55fb47ee908bd2bcf973d1015bbce176f68
tahlil <bsse0803@iit.du.ac.bd>
2022-06-18 12:18:22 +0600

    Setup for devnet deployement

--- programs/solcrowdfunding/src/lib.rs
+++ programs/solcrowdfunding/src/lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::entrypoint::ProgramResult;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("B3kNNQCvmKywbcsHT6z6tNnTD49pijeCupBPKVvUm8rc");
 
 #[program]
 pub mod solcrowdfunding {
